7619f1273040132759b67805ca22e0de20bff87b
Alexandre <alderthault@hotmail.fr>
2023-06-27 13:52:53 +0200

    merge main from Alexandre

--- public/js/register.js
+++ public/js/register.js
@@ -3,6 +3,16 @@ const form = document.querySelector('form');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
+
+        // vérification des mots de passe
+        const password = form.elements['mot_de_passe'].value;
+        const confirmPassword = document.getElementById('confirm-pwd').value;
+    
+        if (password !== confirmPassword) {
+            alert('Les mots de passe ne correspondent pas.');
+            return;  // Terminez tôt pour ne pas envoyer la requête fetch
+        }
+
     const formData = {
         nom: form.elements[0].value,
         prenom: form.elements[1].value,
@@ -13,7 +23,7 @@ form.addEventListener('submit', (e) => {
         type_utilisateur: 'particulier', 
     };
 
-    fetch('http://localhost:3000/create-account', {
+    fetch('/create-account', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -21,37 +31,62 @@ form.addEventListener('submit', (e) => {
         body: JSON.stringify(formData),
     })
     .then(response => {
-        const status = response.status;
-        return response.json().then(data => {
-            return { status: status, body: data }
+        console.log('Response status:', response.status);  
+        console.log('Response headers:', response.headers);  
+        return response.text().then(text => {
+            try {
+
+                const data = JSON.parse(text);
+                console.log('Parsed response data:', data);  
+                return { status: response.status, body: data };
+            } catch (error) {
+                
+                console.log('Raw response text:', text);  
+                throw error;
+            }
         });
     })
-    .then(data => { // à changer pour afficher un msg d'erreur ou de succès
+    .then(data => {
+        console.log('In second .then:', data);  
+    
         if (data.status === 409) {
             showPopup(data.body.message, 'error');
+        } else if (data.status === 200 && data.body.message === 'Account created successfully') {
+            showPopup('Account created successfully', 'success');
+            setTimeout(() => {
+                console.log('Redirecting to index.html');  
+                window.location.href = 'index.html';
+            }, 1000);
         } else if (data.body.message) {
             showPopup(data.body.message, 'error');
-        } else {
-            showPopup('Account created successfully', 'success');
         }
     })
     .catch((error) => {
         console.error('Error:', error);
     });
-    // à changer pr afficher un popup de confirmation
+    
+    
     function showPopup(message, type) {
+        console.log('Showing popup:', message, type);  
+    
         const popup = document.createElement('div');
         popup.classList.add('popup', type);
         popup.textContent = message;
-
+    
         popup.style.position = 'fixed';
         popup.style.right = '20px';
         popup.style.top = '20px';
-
+        popup.style.backgroundColor = type === 'error' ? 'red' : 'green';  
+        popup.style.color = 'white';  
+        popup.style.padding = '20px';  
+        popup.style.borderRadius = '5px';  
+    
         document.body.appendChild(popup);
-
+    
         setTimeout(() => {
+            console.log('Removing popup');  
             popup.remove();
-        }, 3000);
+        }, 1000);
     }
-});
+    
+})
