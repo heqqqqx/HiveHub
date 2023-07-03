34077412f1b0dda2f49d7ef14bf5552494c2dcf4
Mayeul78 <66210853+Mayeul78@users.noreply.github.com>
2023-07-03 12:31:59 +0200

    mini changement style resolution plusieurs petit problems, séparations des fichier css/html/js

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,1 +1,41 @@
-// Récupérer les messages depuis le serve
+function sendMessage(message) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id_utilisateur = urlParams.get('id_utilisateur');
+    const id_autre_utilisateur = urlParams.get('id_autre_utilisateur');
+    console.log(id_utilisateur + "  autre:" + id_autre_utilisateur);
+  
+    fetch('/save-message', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        message: message,
+        id_utilisateur: id_utilisateur,
+        id_autre_utilisateur: id_autre_utilisateur
+      })
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log('Message enregistré avec succès');
+        } else {
+          console.error("Erreur lors de l'enregistrement du message");
+        }
+      })
+      .catch(error => {
+        console.error('Erreur lors de la requête : ' + error);
+      });
+  }
+  
+  document.getElementById('message-form').addEventListener('submit', function (event) {
+    event.preventDefault(); 
+  
+    const messageInput = document.querySelector('.message-input');
+    const message = messageInput.value.trim();
+  
+    if (message) {
+      sendMessage(message);
+      messageInput.value = ''; 
+      location.reload()
+    }
+  });
